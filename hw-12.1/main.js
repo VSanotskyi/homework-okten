cc6a57ec57e79da3bf290eab12c562889af7d695
Vsanotskyi <vsanotskyi@gmail.com>
2023-06-09 04:09:55 +0300

    hw-12.1 end?

--- hw-12.1/main.js
+++ hw-12.1/main.js
@@ -1,51 +1,7 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com";
-const container = document.querySelector(".container-js-hw12");
+const container = document.querySelector(".container-js");
 
 fetchUsers(BASE_URL).then(creatUsersList);
-
-// -------------------------------------------------------
-
-// container.addEventListener("click", showPosts);
-
-function showPosts(evt) {
-    if (evt.target.nodeName !== "BUTTON") return;
-
-    evt.target.disabled = true;
-    const el = evt.target;
-    const id = evt.target.dataset.id;
-
-    fetchPosts(BASE_URL, id).then(posts => {
-        creatPosts(posts, el, id);
-    });
-
-
-    /*if (evt.target.classList.contains("comments-btn")) {
-     const el = evt.target;
-     const id = evt.target.dataset.post;
-
-     fetchComments(BASE_URL, id).then(comments => {
-     creatComments(comments, el);
-     });
-
-     return;
-     }
-
-     if (evt.target.classList.contains("hide-post-btn")) {
-     evt.target.parentElement.classList.add("hide-btn");
-     const id = evt.target.dataset.btnId;
-
-     const arrBtn = evt.currentTarget.getElementsByClassName(
-     "user-post-btn");
-
-     for (const arrEl of arrBtn) {
-     if (id === arrEl.dataset.id) {
-     arrEl.disabled = false;
-     }
-     }
-     }*/
-}
-
-// --------------------------------------------------------
 
 function fetchUsers(url) {
     return fetch(`${url}/users`).then(response => {
@@ -73,6 +29,43 @@ function fetchComments(url, id) {
 
 // --------------------------------------------------------------
 
+function showPosts(evt) {
+    evt.target.disabled = true;
+
+    const el = evt.target;
+    const id = el.dataset.id;
+
+    fetchPosts(BASE_URL, id).then(posts => {
+        creatPosts(posts, el, id);
+    });
+}
+
+function showComments(evt) {
+    const el = evt.target;
+    const id = el.dataset.post;
+
+    fetchComments(BASE_URL, id).then(comments => {
+        creatComments(comments, el);
+    });
+}
+
+function hidePosts(evt) {
+    const el = evt.target;
+    const id = el.dataset.btnId;
+    const arrBtn = container.getElementsByClassName(
+        "user-post-btn");
+
+    el.parentElement.classList.add("hide-btn");
+
+    for (const btnEl of arrBtn) {
+        if (id === btnEl.dataset.id) {
+            btnEl.disabled = false;
+        }
+    }
+}
+
+// --------------------------------------------------------
+
 function creatUsersList(users) {
     const ul = document.createElement("ul");
 
@@ -90,20 +83,9 @@ function creatUsersList(users) {
         btn.dataset.id = `${user.id}`;
 
         p.innerText = `Name: ${user.name}, ID: ${user.id}`;
+        btn.innerText = "Show posts";
 
-        btn.addEventListener("click", (evt) => {
-            evt.target.disabled = true;
-            const el = evt.target;
-            const id = evt.target.dataset.id;
-
-            fetchPosts(BASE_URL, id).then(posts => {
-                creatPosts(posts, el, id);
-            });
-        });
-        // li.innerHTML = `
-        // <p class="user-text">Name: ${user.name}, ID:
-        // ${user.id}</p> <button class="user-post-btn" data-id="${user.id}"
-        // type="button">Show posts</button>`;
+        btn.addEventListener("click", showPosts);
 
         li.append(p, btn);
         ul.appendChild(li);
@@ -114,31 +96,38 @@ function creatUsersList(users) {
 
 function creatPosts(posts, el, id) {
     const div = document.createElement("div");
-    const postList = document.createElement("ul");
-    const btn = document.createElement("button");
+    const ul = document.createElement("ul");
+    const btnHidePost = document.createElement("button");
 
-    postList.classList.add("post-list");
-    btn.type = "button";
-    btn.innerText = "Hide posts";
-    btn.classList.add("hide-post-btn", "btn");
-    btn.dataset.btnId = `${id}`;
+    ul.classList.add("post-list");
+    btnHidePost.innerText = "Hide posts";
+    btnHidePost.classList.add("hide-post-btn", "btn");
+
+    btnHidePost.dataset.btnId = `${id}`;
+
+    btnHidePost.addEventListener("click", hidePosts);
 
     posts.map(post => {
-        const postItem = document.createElement("li");
-        postItem.classList.add("post-item");
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        const btn = document.createElement("button");
 
-        postItem.innerHTML = `<h3 class="post-title">${post.title}</h3>
-                                <p class="post-text">${post.body}</p>
-                                <button data-post="${post.id}" 
-                                        class="comments-btn" 
-                                        type="button">
-                                    Show comments
-                                </button>`;
+        li.classList.add("post-item");
+        p.classList.add("post-text");
+        btn.classList.add("comments-btn");
 
-        postList.append(postItem);
+        btn.dataset.post = `${post.id}`;
+
+        p.innerText = `${post.body}`;
+        btn.innerText = "Show comments";
+
+        btn.addEventListener("click", showComments);
+
+        li.append(p, btn);
+        ul.appendChild(li);
     });
 
-    div.append(postList, btn);
+    div.append(ul, btnHidePost);
     el.after(div);
 }
 
